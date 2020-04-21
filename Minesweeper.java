ae8129f018a9e9a2149947bd742f573ab6a3a450
pperone <pperone@gmail.com>
2020-04-21 14:21:03 -0300

    Remove unused import

--- Minesweeper.java
+++ Minesweeper.java
@@ -3,7 +3,6 @@ import java.awt.EventQueue;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
